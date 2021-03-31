40a3e6a0913c7e4d102b78d1ec041a3d5deb729e
Elias Soares <eliassoaressouza@gmail.com>
2021-03-31 18:32:49 -0300

    ajustes

--- src/app/shared/graficopizza/graficopizza.component.ts
+++ src/app/shared/graficopizza/graficopizza.component.ts
@@ -1,49 +1,54 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 declare var google: any;
+import * as $ from "jquery";
 @Component({
-  selector: 'graficopizza',
-  templateUrl: './graficopizza.component.html',
-  styleUrls: ['./graficopizza.component.css']
+  selector: "graficopizza",
+  templateUrl: "./graficopizza.component.html",
+  styleUrls: ["./graficopizza.component.css"],
 })
-export class GraficopizzaComponent implements OnInit, AfterViewInit{
-  @ViewChild('pieChart', { static: false }) pieChart: ElementRef
-  constructor() { }
+export class GraficopizzaComponent implements OnInit, AfterViewInit {
+  @ViewChild("pieChart", { static: false }) pieChart: ElementRef;
+  constructor() {}
 
-  @Input()datain=[
-    ['classificacao', 'valor'],
-    ['Work', 11],
-    ['Eat', 2],
-    ['Commute', 2],
-    ['Watch TV', 2],
-    ['Sleep', 7]
+  @Input() datain = [
+    ["classificacao", "valor"],
+    ["Work", 11],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
   ];
-  ngOnInit() {
-  }
+  ngOnInit() {}
   drawChart = () => {
-
-    const data = google.visualization.arrayToDataTable([
-      ['classificacao', 'valor'],
-      ['Work', 11],
-      ['Eat', 2],
-      ['Commute', 2],
-      ['Watch TV', 2],
-      ['Sleep', 7]
-    ]);
+    console.log('##########################################this.datain####################')
+    console.log(this.datain)
+    const data = google.visualization.arrayToDataTable(JSON.parse(JSON.stringify(this.datain)));
 
     const options = {
-      title: 'Classificacao',
-      legend: {position: 'top'},
-      dataType: "json"
+      title: "Classificacao",
+      legend: { position: "top" },
+      dataType: "json",
     };
 
-    const chart = new google.visualization.PieChart(this.pieChart.nativeElement);
+    const chart = new google.visualization.PieChart(
+      this.pieChart.nativeElement
+    );
 
     chart.draw(data, options);
-  }
+  };
 
-    ngAfterViewInit() {
-      google.charts.load('current', { 'packages': ['corechart'] });
+  ngAfterViewInit() {
+
+      google.charts.load("current", { packages: ["corechart"] });
+
       google.charts.setOnLoadCallback(this.drawChart);
-    }
 
+  }
 }
